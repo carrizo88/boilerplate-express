3b74692d74cd433c678905fe14fa7e85078fde98
Franco Carrizo <francarri_6@hotmail.com>
2024-04-26 07:08:15 +0000

    adding frist steps

--- myApp.js
+++ myApp.js
@@ -1,5 +1,24 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+app.use("/public", express.static(__dirname + '/public'))
+
+app.get("/",(req,res)=> {
+
+const absolutePath = __dirname +'/views/index.html'
+    res.sendFile(absolutePath);
+})
+console.log("sape",process.env.MESSAGE_STYLE)
+app.use((req,res,next)=>{
+    const mesj = req.method +" "+req.path+' - ::ffff:'+req.ip
+    console.log(mesj);
+    next();
+})
+app.get("/json",(req,res)=> {
+const response= {"message": process.env.MESSAGE_STYLE==='uppercase' ? "HELLO JSON":"Hello json"}
+    res.json(response)
+    })
 
 
 
@@ -25,15 +44,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
