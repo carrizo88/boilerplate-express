88bb4894cfe951c0b1289c6e866ea2ca73ef9c49
Franco Carrizo <francarri_6@hotmail.com>
2024-04-27 06:24:23 +0000

    final modules

--- myApp.js
+++ myApp.js
@@ -1,34 +1,53 @@
 let express = require('express');
 let app = express();
 require('dotenv').config()
-
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
 app.use("/public", express.static(__dirname + '/public'))
 
-app.get("/",(req,res)=> {
-
-const absolutePath = __dirname +'/views/index.html'
+app.get("/", (req, res) => {
+    const absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath);
 })
-console.log("sape",process.env.MESSAGE_STYLE)
-app.use((req,res,next)=>{
-    const mesj = req.method +" "+req.path+' - ::ffff:'+req.ip
+
+app.use((req, res, next) => {
+    const mesj = req.method + " " + req.path + ' - ::ffff:' + req.ip
     console.log(mesj);
     next();
 })
-app.get("/json",(req,res)=> {
-const response= {"message": process.env.MESSAGE_STYLE==='uppercase' ? "HELLO JSON":"Hello json"}
+
+app.get("/json", (req, res) => {
+    const response = { "message": process.env.MESSAGE_STYLE === 'uppercase' ? "HELLO JSON" : "Hello json" }
     res.json(response)
-    })
+})
 
 
+app.get('/now',(req, res, next) => {
+    const timestamp = new Date().toString()
+    console.log(timestamp);
+    req.time=timestamp
+    next();
+},(req,res) => {
+    res.json({time: req.time})
+})
 
+app.get('/:word/echo',(req,res)=> {
+    res.json({echo: req.params.word})
+})
 
+app.get("/name", (req, res) => {
+    console.log('req.query',req.query)
+    const {first, last} =  req.query
+    const resp={ name: `${first} ${last}`}
+    res.json(resp)
+})
 
-
-
-
-
-
+app.post("/name", (req, res) => {
+    console.log('req',req.body)
+    const {first, last} =  req.body
+    const resp={ name: `${first} ${last}`}
+    res.json(resp)
+})
 
 
 
